e4ac6965cb66f2c71f3b2fde1dc1a6b9d8ffed27
Bruneel Robin <robin.bruneel@provikmo.be>
2017-05-12 09:53:52 +0200

    angular poc

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,28 +1,24 @@
 "use strict";
-var Reservation = (function () {
-    function Reservation() {
-    }
-    return Reservation;
-}());
-exports.Reservation = Reservation;
-var RESERVATIONS = [
-    { id: 11, date: '05/05/2016', startHour: '12:00', duur: '30' },
-    { id: 11, date: '05/05/2016', startHour: '12:00', duur: '30' },
-    { id: 11, date: '05/05/2016', startHour: '12:00', duur: '30' },
-    { id: 11, date: '05/05/2016', startHour: '12:00', duur: '30' },
-];
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = 'Reservaties';
-        this.reservations = RESERVATIONS;
-        this.reservation = {
-            id: 1,
-            date: '05/05/2016',
-            startHour: '12:00',
-            duur: '30'
-        };
+        this.title = 'Tour of Heroes';
     }
     return AppComponent;
 }());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'my-app',
+        template: "\n    <h1>{{title}}</h1>\n    <nav>\n      <a routerLink=\"/dashboard\" routerLinkActive=\"active\">Dashboard</a>\n      <a routerLink=\"/heroes\" routerLinkActive=\"active\">Heroes</a>\n    </nav>\n    <router-outlet></router-outlet>\n  ",
+        styleUrls: ['./app.component.css']
+    })
+], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
